0a64f9800f42915a3dbb9527737c4cdee85bab28
Efosee <valeraparaskev@gmail.com>
2025-02-26 02:51:39 +0300

    add hooks useEffect and useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { Component, useState, useRef, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
@@ -48,22 +48,16 @@ class SliderFromClass extends Component {
 	}
 }
 
+const calcValue = () => {
+	console.log('random');
+
+	return Math.random() * (50 - 1 + 1) + 1;
+}
+
 // Функциональный компонент с использованием хука useState (альтернатива компонента выше)
 const SliderFromFunc = (props) => {
 
-	/* Создание нескольких переменных и функций для управления состояниями:
-	const [slide, setSlide] = useState(0);
-	const [autoplay, setAutoplay] = useState(true);
-
-	function changeSlide(i) {
-		setSlide(slide => slide + i);
-	}
-
-	function toggleAutoplay() {
-		setAutoplay(autoplay => !autoplay)
-	} */
-
-	// Создание состояния с объектом (по аналогии как state в классовом объекте)
+	/* Создание состояния с объектом (по аналогии как state в классовом объекте)
 	const [state, setState] = useState({slide: 0, autoplay: false});
 
 	function changeSlide(i) {
@@ -72,13 +66,31 @@ const SliderFromFunc = (props) => {
 
 	function toggleAutoplay() {
 		setState(state => ({...state, autoplay: !state.autoplay}));
+	} */
+
+	// Создание нескольких переменных и функций для управления состояниями:
+	const [slide, setSlide] = useState(calcValue);
+	const [autoplay, setAutoplay] = useState(true);
+	const countNumberOfRender = useRef(1);
+
+	useEffect(() => {
+		countNumberOfRender++;
+		console.log(countNumberOfRender);
+	})
+
+	function changeSlide(i) {
+		setSlide(slide => slide + i);
 	}
+
+	function toggleAutoplay() {
+		setAutoplay(autoplay => !autoplay)
+	} 
 
 	return (
 		<Container>
 			<div className="slider w-50 m-auto">
 				<img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-				<div className="text-center mt-5">Active slide {state.slide} <br /> {state.autoplay ? 'auto' : null}</div>
+				<div className="text-center mt-5">Active slide {slide} <br /> {autoplay ? 'auto' : null}</div>
 				<div className="buttons mt-3">
 					<button
 						className="btn btn-primary me-2"
